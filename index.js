35645662f24f8bd8af47558d0a6922a29e6cf057
ftgyhuji8 <gs1purdyboo@hotmail.com>
2024-05-07 23:27:47 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
-    }
-  }
-  return dp[0];
-}
+const minN = (arr, n = 1) => [...arr].sort((a, b) => a - b).slice(0, n);
